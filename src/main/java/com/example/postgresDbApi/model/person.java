9e9cfb8821beb47fb0bb30bb7db04dcf6f1aa17f
alexYamaoka <ryamaoka1@gmail.com>
2020-06-22 18:38:58 -0700

    added person class

--- src/main/java/com/example/postgresDbApi/model/person.java
+++ src/main/java/com/example/postgresDbApi/model/person.java
@@ -1,6 +1,25 @@
 package com.example.postgresDbApi.model;
 
+import java.util.UUID;
+
 public class person
 {
-    
+    private UUID id;
+    private String name;
+
+    public person(UUID id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
 }
