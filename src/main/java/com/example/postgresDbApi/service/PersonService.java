4d28ef364033872ed83ba987e56547a7681ecd37
alexYamaoka <ryamaoka1@gmail.com>
2020-06-22 19:17:50 -0700

    added person service class to connect personDAO

---
+++ src/main/java/com/example/postgresDbApi/service/PersonService.java
@@ -0,0 +1,43 @@
+package com.example.postgresDbApi.service;
+
+import com.example.postgresDbApi.dao.PersonDao;
+import com.example.postgresDbApi.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class PersonService
+{
+    private PersonDao personDao;
+
+    public PersonService(PersonDao personDao)
+    {
+        this.personDao = personDao;
+    }
+
+    public boolean addPerson(Person person)
+    {
+        return personDao.insertPerson(person);
+    }
+
+    public Optional<Person> getPersonById(UUID id)
+    {
+        return personDao.selectPersonById(id);
+    }
+
+    public List<Person> getAllPeople()
+    {
+        return personDao.selectAllPeople();
+    }
+
+    public boolean updatePerson(UUID id, Person person)
+    {
+        return personDao.updatePersonById(id, person);
+    }
+
+    public boolean deletePerson(UUID id)
+    {
+        return personDao.deletePersonById(id);
+    }
+}
